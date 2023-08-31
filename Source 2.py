3ec96ca45ad75ffb96904daeb4882fb1aa89cf4d
rahmadmukt <143482814+rahmadmukt@users.noreply.github.com>
2023-08-31 11:18:29 +0700

    Add files via upload

---
+++ Source 2.py
@@ -0,0 +1,5 @@
+m = ['Alice', 'ants', 'Bob', 'badgers', 'Carol', 'cats']
+m.sort()
+print(m)
+spam=list(map(str.lower,['a', 'z', 'A', 'Z']))
+print(spam)
---
+++ Source 6.py
@@ -0,0 +1,9 @@
+a ='''Dear Alice, 
+How have you been? I am fine.
+There is a container in the fridge
+that is labeled "Milk Experiment".
+Please do not drink it.
+Sincerely,
+Bob'''
+hasil = a.split('\n')
+print(hasil)
